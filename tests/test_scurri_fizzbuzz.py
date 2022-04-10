bf03d8d8a6b79ed08982605905138ac39c8e2982
mcsuibhne <jonathan.sweeney@ucdconnect.ie>
2022-04-10 18:38:10 +0100

    Refactor tests to improve readability and test coverage

--- tests/test_scurri_fizzbuzz.py
+++ tests/test_scurri_fizzbuzz.py
@@ -3,17 +3,25 @@ from scurrifizzbuzz import scurri_fizzbuzz
 
 
 class FizzBuzzTests(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(scurri_fizzbuzz.fizzbuzz(2), 2)
-        self.assertEqual(scurri_fizzbuzz.fizzbuzz(3), "Three")
-        self.assertEqual(scurri_fizzbuzz.fizzbuzz(5), "Five")
-        self.assertEqual(scurri_fizzbuzz.fizzbuzz(15), "ThreeFive")
+    """Unit tests for scurri_fizzbuzz.py """
+
+    def test_three_multiples(self):
+        for i in [3, 12, 33, 51, 81]:
+            self.assertEqual(scurri_fizzbuzz.fizzbuzz(i), "Three")
 
 
+    def test_five_multiples(self):
+        for i in [5, 25, 55, 70, 95]:
+            self.assertEqual(scurri_fizzbuzz.fizzbuzz(i), "Five")
 
 
+    def test_fifteen_multiples(self):
+        for i in [15, 45, 60, 75, 90]:
+            self.assertEqual(scurri_fizzbuzz.fizzbuzz(i), "ThreeFive")
 
-
+    def test_non_threefive_multiples(self):
+        for i in [1, 4, 43, 67, 82]:
+            self.assertEqual(scurri_fizzbuzz.fizzbuzz(i), i)
 
 if __name__ == '__main__':
     unittest.main()
